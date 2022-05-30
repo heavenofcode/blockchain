7ad75d2adb9b43ed86767e7d2549560d56efeadd
heavenofcode <heavenofcode@gmail.com>
2022-05-30 17:32:03 +0700

    update source code

--- Quizz.sol
+++ Quizz.sol
@@ -31,14 +31,17 @@ contract Quizz {
         string name;
         string email;
         string mobile_number;
-        address addr;
     }
 
     constructor(){
         numOfQuestions = 0;
         correctAnswer = 0;
         owner = msg.sender;
+
+        // create default test
         createTest(1, "Math", 60, 10);
+
+        // create default question, you can add other question by use addQuestion function
         addQuestion(1, 1, "1 + 1 = ?", "1", "2", "3", "b");
         addQuestion(1, 2, "1 + 2 = ?", "1", "2", "3", "c");
     }
@@ -46,11 +49,14 @@ contract Quizz {
     mapping(uint => Question) quests;
 
     Test test;
+    // only owner can acccess this function
     function createTest(uint _id, string memory _name, uint _minimumScore, uint _reward) public isOwner {
         numOfTest++;
         test = Test(_id, _name, _minimumScore, _reward);
     }
 
+    // only owner can acccess this function
+    // require to create test first before use this function
     function addQuestion(uint _testId, uint _number, string memory _quest,string memory _optA, string memory _optB, string memory _optC, string memory _optKey) public isOwner {
         require(numOfTest > 0, "Plase create Test first");
         numOfQuestions++;
@@ -58,11 +64,14 @@ contract Quizz {
     }
 
     Participant participant;
-    function registration(string memory _name, string memory _email, string memory _mobile_number, address payable _addr) public isParticipant {
-        participant = Participant(_name, _email, _mobile_number, _addr);
-        participant.addr = msg.sender;
+    // only participant can acccess this function
+    // rate is 2 ether
+    function registration(string memory _name, string memory _email, string memory _mobile_number) payable external isParticipant checkQuizRates(2 ether) {
+        participant = Participant(_name, _email, _mobile_number);
+        owner.transfer(msg.value);
     }
-    
+
+    // show question of test by number of question    
     function showQuestion(uint _number) view public returns (uint, string memory, string memory, string memory, string memory) {
         Question memory q = quests[_number];
         return (
@@ -74,6 +83,7 @@ contract Quizz {
         );
     }
 
+    // only participant can acccess this function
     function startTest(uint _number, string memory _answer) public isParticipant {
         require(_number > 0 && _number <= numOfQuestions);
         if (keccak256(abi.encodePacked(_answer)) == keccak256(abi.encodePacked(quests[_number].optKey))) {
@@ -81,14 +91,12 @@ contract Quizz {
         }
     }
 
+    // checking for correct answer
     function testChecking() payable external {
         uint score = 0;
         score = correctAnswer / numOfQuestions * 100;
         if(score >= test.minimumScore) {
             result = string(abi.encodePacked("Congratulation ", participant.name, ", You passed the test!"));
-            // participant.addr.transfer(10 ether);
-            // msg.sender.transfer(test.reward);
-            // address(participant.addr).transfer(test.reward);
         } else {
             result = string(abi.encodePacked("Sorry ", participant.name, ", You can try again later"));
         }
@@ -106,6 +114,11 @@ contract Quizz {
 
     modifier isParticipant {
         require (owner != msg.sender, "Only participant can access this function");
+        _;
+    }
+
+    modifier checkQuizRates(uint _rates) {
+        require (msg.value >= _rates, "Insufficient payment, the rate is 2 ether");
         _;
     }
 }
